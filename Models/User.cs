cb419ff9651f083200af8e6a6973e14c189ec53e
Jordan Forbes <jordforb@gmail.com>
2022-06-13 13:16:10 -0400

    iniital commit

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Moth_Note.Models
+{
+	public class User
+	{
+		public User(int Id, string FirstName, string LastName, string Email)
+		{
+			this.Id = Id;
+			this.FirstName = FirstName;
+			this.LastName = LastName;
+			this.Email = Email;
+		}
+
+		public int Id { get; set; }
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+		public string? Email { get; set; }
+	}
+}
+
---
+++ Models/UserClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Moth_Note.Models
+{
+	public class UserModel
+	{
+		public UserModel()
+		{
+		}
+	}
+}
+
